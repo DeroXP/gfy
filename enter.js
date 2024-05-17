e7acd21bac78f7d160a995449f26e658f1eb22d5
Derourn <96676237+DeroXP@users.noreply.github.com>
2024-05-17 18:30:40 -0500

    Update enter.js

--- enter.js
+++ enter.js
@@ -59,7 +59,7 @@ function createChatModal(chatUrl) {
     chatModal.style.left = '50%';
     chatModal.style.transform = 'translate(-50%, -50%)';
     chatModal.style.width = '1024px';
-    chatModal.style.height = '576pxpx';
+    chatModal.style.height = '576px';
     chatModal.style.zIndex = '10000';
     chatModal.style.backgroundColor = '#2b2b2b';
     chatModal.style.borderRadius = '10px';
